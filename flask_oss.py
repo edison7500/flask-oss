8038fdd65fd83f7ba09d27f4777d33daf365a8e9
jiaxin <edison7500@gmail.com>
2016-07-27 14:29:24 +0800

    merge

---
+++ flask_oss.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import hashlib
+import json
+import logging
+import os
+import re
+import gzip
+
+import warnings
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO
+import mimetypes
+from collections import defaultdict
+
+import oss2
+import oss2.exceptions
+from oss2.exceptions import ClientError
+from flask import current_app
+from flask import url_for as flask_url_for
+import six
+
+
+logger = logging.getLogger('flask_oss')
+
+
+DEFAULT_SETTINGS = {'FLASKOSS_USE_HTTPS': True,
+                    'FLASKOSS_ACTIVE': True,
+                    'FLASKOSS_DEBUG': False,
+                    'FLASKOSS_BUCKET_DOMAIN': 's3.amazonaws.com',
+                    'FLASKOSS_CDN_DOMAIN': '',
+                    'FLASKOSS_USE_CACHE_CONTROL': False,
+                    'FLASKOSS_HEADERS': {},
+                    'FLASKOSS_FILEPATH_HEADERS': {},
+                    'FLASKOSS_ONLY_MODIFIED': False,
+                    'FLASKOSS_URL_STYLE': 'host',
+                    'FLASKOSS_GZIP': False,
+                    'FLASKOSS_GZIP_ONLY_EXTS': [],
+                    'FLASKOSS_FORCE_MIMETYPE': False,
+                    }
+
+__version__ = (0, 1, 0)
+
+
+
+class FlaskOSS(object):
+
+
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """
+        An alternative way to pass your :class:`flask.Flask` application
+        object to Flask-S3. :meth:`init_app` also takes care of some
+        default `settings`_.
+        :param app: the :class:`flask.Flask` application object.
+        """
+
+        for k, v in DEFAULT_SETTINGS.items():
+            app.config.setdefault(k, v)
+
+        # print app.config
+
---
+++ setup.py
@@ -0,0 +1,55 @@
+"""
+Flask-OSS
+-------------
+Easily serve your static files from aliyun oss.
+"""
+from setuptools import setup
+
+
+def parse_version(asignee):
+    import os, re
+    here = os.path.dirname(os.path.abspath(__file__))
+    version_re = re.compile(
+        r'%s = (\(.*?\))' % asignee)
+    with open(os.path.join(here, 'flask_oss.py')) as fp:
+        for line in fp:
+            match = version_re.search(line)
+            if match:
+                version = eval(match.group(1))
+                return ".".join(map(str, version))
+        else:
+            raise Exception("cannot find version")
+version = parse_version('__version__')
+
+
+
+setup(
+    name='Flask-OSS',
+    version=version,
+    # url='http://github.com/e-dard/flask-s3',
+    license='WTFPL',
+    author='jiaxin',
+    author_email='jiaxin@guoku.com',
+    description='Seamlessly serve the static files of your Flask app from aliyun oss',
+    long_description=__doc__,
+    py_modules=['flask_oss'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'oss2>=2.0.0',
+        'six'
+    ],
+    tests_require=['nose', 'mock'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite = 'nose.collector'
+)
