f34c21969f0c5cff5c983b5034a3c0a47c3fd0c3
jiaxin <edison7500@gmail.com>
2019-10-10 12:14:55 +0800

    run code style black

--- flask_oss.py
+++ flask_oss.py
@@ -10,41 +10,38 @@ import oss2
 import oss2.exceptions
 
 
-logger = logging.getLogger('flask_oss')
+logger = logging.getLogger("flask_oss")
 
 
 __version__ = (0, 2, 0)
 
 
-
 class FlaskOSS(object):
-
-
     def __init__(self, app=None):
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
 
-        _access_key     = app.config.get('OSS_ACCESS_KEY_ID')
-        _secret         = app.config.get('OSS_SECRET_ACCESS_KEY')
-        _endpoint       = app.config.get('OSS_ENDPOINT')
-        _bucket_name    = app.config.get('OSS_BUCKET_NAME')
+        _access_key = app.config.get("OSS_ACCESS_KEY_ID")
+        _secret = app.config.get("OSS_SECRET_ACCESS_KEY")
+        _endpoint = app.config.get("OSS_ENDPOINT")
+        _bucket_name = app.config.get("OSS_BUCKET_NAME")
         # assert self.access_key is not None
         # print self.access_key
         # if self.access_key is None or self.secret is None:
         #     raise
-        self.auth   = oss2.Auth(_access_key, _secret)
+        self.auth = oss2.Auth(_access_key, _secret)
 
         self.bucket = oss2.Bucket(self.auth, _endpoint, _bucket_name)
 
     def put_file(self, filename=None, raw_contents=None):
         assert filename is not None
         success = self.bucket.put_object(filename, raw_contents)
-        if (success.status == 200):
+        if success.status == 200:
             return filename
         else:
-            logger.error("FAILURE writing file {filename}".format(filename= filename))
+            logger.error("FAILURE writing file {filename}".format(filename=filename))
 
     def get_file(self, filename=None):
         assert filename is not None
@@ -53,11 +50,15 @@ class FlaskOSS(object):
             result = self.bucket.get_object(filename)
             return result.read()
         except oss2.exceptions.NoSuchKey as e:
-            logger.error('{0} not found: http_status={1}, request_id={2}'.format(filename, e.status, e.request_id))
+            logger.error(
+                "{0} not found: http_status={1}, request_id={2}".format(
+                    filename, e.status, e.request_id
+                )
+            )
 
     def exists_file(self, filename=None):
         assert filename is not None
-        exist  = self.bucket.object_exists(filename)
+        exist = self.bucket.object_exists(filename)
         if exist:
             return True
 
@@ -70,9 +71,10 @@ class FlaskOSS(object):
             self.bucket.delete_object(filename)
             is_delete = True
         except oss2.exceptions.NoSuchKey as e:
-            logger.error('{0} not found: http_status={1}, request_id={2}'.format(filename, e.status, e.request_id))
+            logger.error(
+                "{0} not found: http_status={1}, request_id={2}".format(
+                    filename, e.status, e.request_id
+                )
+            )
         finally:
             return is_delete
-
-
-
--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ from setuptools import setup
 
 def parse_version(asignee):
     import os, re
+
     here = os.path.dirname(os.path.abspath(__file__))
-    version_re = re.compile(
-        r'%s = (\(.*?\))' % asignee)
-    with open(os.path.join(here, 'flask_oss.py')) as fp:
+    version_re = re.compile(r"%s = (\(.*?\))" % asignee)
+    with open(os.path.join(here, "flask_oss.py")) as fp:
         for line in fp:
             match = version_re.search(line)
             if match:
@@ -19,37 +19,34 @@ def parse_version(asignee):
                 return ".".join(map(str, version))
         else:
             raise Exception("cannot find version")
-version = parse_version('__version__')
 
+
+version = parse_version("__version__")
 
 
 setup(
-    name='Flask-OSS',
+    name="Flask-OSS",
     version=version,
-    url='https://github.com/edison7500/flask-oss',
-    license='WTFPL',
-    author='jiaxin',
-    author_email='jiaxin@guoku.com',
-    description='Seamlessly serve the static files of your Flask app from aliyun oss',
+    url="https://github.com/edison7500/flask-oss",
+    license="WTFPL",
+    author="jiaxin",
+    author_email="jiaxin@guoku.com",
+    description="Seamlessly serve the static files of your Flask app from aliyun oss",
     long_description=__doc__,
-    py_modules=['flask_oss'],
+    py_modules=["flask_oss"],
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
-    install_requires=[
-        'Flask',
-        'oss2>=2.0.0',
-        'six'
-    ],
-    tests_require=['nose', 'mock'],
+    platforms="any",
+    install_requires=["Flask", "oss2>=2.0.0", "six"],
+    tests_require=["nose", "mock"],
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: Other/Proprietary License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    test_suite = 'nose.collector'
+    test_suite="nose.collector",
 )
